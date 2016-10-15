25a51e3d8d8efe01b7153abc1c6af6ae11c17e0f
Toby <tobyond@gmail.com>
2016-10-15 13:06:40 -0700

    added foreman and the rake task to spin both servers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,24 @@
 class CommentsController < ApplicationController
   def index
+    render(
+      status: 200,
+      json: Comment.all
+    )
   end
+
+  def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      render json: comment, status: 201
+    else
+      render json: { errors: comment.errors }, status: 422
+    end
+  end
+
+  private
+
+  def comment_params
+    params.permit(:id, :author, :text)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'comments/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    get :comments, to: 'comments#index'
+    post :comments, to: 'comments#create'
+  end
 end
